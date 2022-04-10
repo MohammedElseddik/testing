88f31f96e9b3387a6f468b7578708c24079b3b6e
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-10 02:20:24 +0200

    Add the test for reverseString function

---
+++ reverseString.test.js
@@ -0,0 +1,5 @@
+const reverseString = require("./reverseString.js");
+
+test("It should return the opposite of javascript is tpircsavaj", () => {
+    expect(reverseString("javascript")).toBe("tpircsavaj");
+});
